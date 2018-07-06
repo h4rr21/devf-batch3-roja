7533cda7d5ff8d8def5088e3c207ad1fe8d0f8b1
Juan Carlos <h4rry@Juans-MacBook-Pro.local>
2018-07-05 21:39:23 -0500

    firebase database and logs

--- node/firebaseAuthFront/js/dashboard.js
+++ node/firebaseAuthFront/js/dashboard.js
@@ -1,18 +1,21 @@
 var config = {
-    apiKey: "AIzaSyC5Nh_lts0ekTcOBd2gdueBIpuhaJnv3t0",
-    authDomain: "full-firebase-4a4b3.firebaseapp.com",
-    databaseURL: "https://full-firebase-4a4b3.firebaseio.com",
-    projectId: "full-firebase-4a4b3",
-    storageBucket: "full-firebase-4a4b3.appspot.com",
-    messagingSenderId: "870011553445"
-  };
+  apiKey: "AIzaSyBPUXuAEjquJrNkkE7QANj87lsC6hdH8Oo",
+  authDomain: "social-media-b6a2d.firebaseapp.com",
+  databaseURL: "https://social-media-b6a2d.firebaseio.com",
+  projectId: "social-media-b6a2d",
+  storageBucket: "social-media-b6a2d.appspot.com",
+  messagingSenderId: "816678703841"
+};
+
 firebase.initializeApp(config);
+var currentUser = {};
 
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
       document.getElementById('user-name').textContent=user.displayName;
       document.getElementById('user-image').setAttribute('src',user.photoURL);
+      currentUser=user;
       // ...
     } else {
       window.location="index.html";
@@ -28,3 +31,30 @@ firebase.auth().onAuthStateChanged(function(user) {
         console.log('something went wrong',error);
       });
   }
+
+function postSomething(){
+  let postText = document.getElementById('newPostInput').value;
+
+  if (postText.length>0){
+    firebase.database().ref('posts/'+new Date().getTime()).set({
+      username: currentUser.displayName,
+      photo: currentUser.photoURL,
+      post:postText,
+      currentUser: new Date().toISOString()
+    });
+    document.getElementById('newPostInput').value="";
+  }
+}
+
+firebase.database().ref('posts/').on('child_added',snapshot=>{
+  console.log(snapshot.val());
+  let newPost = document.getElementById('emptyPost').cloneNode(true);
+  newPost.setAttribute('style','');
+
+  newPost.querySelector('img').setAttribute('src',snapshot.val().photo);
+  newPost.querySelector('h5').textContent=snapshot.val().username;
+  newPost.querySelector('h3').textContent=snapshot.val().post;
+
+  document.getElementById('posts-container').prepend(newPost);
+
+})
--- node/firebaseAuthFront/js/signup.js
+++ node/firebaseAuthFront/js/signup.js
@@ -1,16 +1,17 @@
 var config = {
-    apiKey: "AIzaSyC5Nh_lts0ekTcOBd2gdueBIpuhaJnv3t0",
-    authDomain: "full-firebase-4a4b3.firebaseapp.com",
-    databaseURL: "https://full-firebase-4a4b3.firebaseio.com",
-    projectId: "full-firebase-4a4b3",
-    storageBucket: "full-firebase-4a4b3.appspot.com",
-    messagingSenderId: "870011553445"
+    apiKey: "AIzaSyBPUXuAEjquJrNkkE7QANj87lsC6hdH8Oo",
+    authDomain: "social-media-b6a2d.firebaseapp.com",
+    databaseURL: "https://social-media-b6a2d.firebaseio.com",
+    projectId: "social-media-b6a2d",
+    storageBucket: "social-media-b6a2d.appspot.com",
+    messagingSenderId: "816678703841"
   };
+
 firebase.initializeApp(config);
  
 function signUpEmail(){
     let email=document.getElementById('email').value;
-    let pass=document.getElementById('passeord').value;
+    let pass=document.getElementById('password').value;
 
     if(email.length>0 && pass.length>0){
         firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
